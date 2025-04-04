f530128d5dcfb907e7ba03a4b88e547899f57c30
swisd <glordi053@gmail.com>
2025-04-04 17:22:33 -0400

    it works now kinda sorta

---
+++ LightCommander.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtCore import QFile, QTextStream
+from ui_lightcmdr import Ui_MainWindow
+import qdarktheme
+app = QApplication(sys.argv)
+#app.setStyleSheet(qdarktheme.load_stylesheet())
+file = QFile("stylesheet.qss")
+file.open(QFile.ReadOnly | QFile.Text)
+stream = QTextStream(file)
+app.setStyleSheet(stream.readAll())
+window = QMainWindow()
+form = Ui_MainWindow()
+#form.setDocumentMode(True)
+form.setupUi(window)
+window.show()
+sys.exit(app.exec_())
